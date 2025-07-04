8d0a00432c000e7ac2788ccda53fdf48128ff837
alesandrshy <alesandrshyti454@gmail.com>
2025-07-04 08:52:35 +0000

    deploy V0.1.2.7

--- index.f960d.js
+++ index.f960d.js
@@ -1,4 +1,4 @@
-System.register(["./application.dd09c.js"], function (_export, _context) {
+System.register(["./application.edf04.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
